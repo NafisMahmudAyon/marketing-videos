544a4a5d9b24bc8f8531d7713836221c2a36c131
NafisMahmudAyon <nafismahmudayon@gmail.com.com>
2024-01-27 21:02:04 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
 			id: 9,
 			title: "মাত্র ১ দিনে ১০ লাখ টাকা সেল করার চ্যালেঞ্জ - ক্লাস ০৯",
 			desc: "Explore the fundamental principles of data science.",
-			url: "/video-09.mp4",
+			url: "https://marketing-video.nafisbd.com/video-09.mp4",
 			tags: ["data science", "analytics", "statistics"],
 			videoLength: "22:45",
 		},
@@ -166,6 +166,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
